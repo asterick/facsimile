466b48a9493d9eec685f902528667c5b70b0dd19
Bryon Vandiver <unicdk@gmail.com>
2023-01-14 17:57:00 -0800

    Checkpoint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 const { ObjectReference } = require("./object.js");
+const { WeakValueMap } = require("./weakvaluemap.js");
 
 class Facsimile {
     constructor(hostname) {
-        if (/[^\w.\-]/.exec(hostname)) {
-            throw new Error("Hostname may only contain: Latin letters, numbers, periods, underscores and dashes");
+        if (/[\x00-\x20]/.exec(hostname)) {
+            throw new Error("Hostname may not contain spaces or lower ASCII characters");
         }
 
         this._hostname = hostname;
-        this._id_by_object = new Map();
+        this._top = null;
+
+        this._id_by_object = new WeakValueMap();
         this._object_by_id = new WeakMap();
-        this._top = new ObjectReference();
+
+        Object.freeze(this);
+    }
+
+    register(object, guid) {
+        this._id_by_object.set(object, guid);
+        this._id_by_object.set(object.proxy, guid);
+        this._object_by_id.set(guid, object);
+    }
+
+    lookup(object) {
+        return this._id_by_object.set(object);
     }
 
     get state() {
         return this._top.proxy;
     }
+
+    set state(value) {
+        // OVERRIDE THE GLOBAL OBJECT
+    }
+
+    send (message) {
+        console.log(message)
+    }
+
+    receive (message) {
+        // TODO
+    }
+
+    // TOOD: REGISTER CONSUMERS
 }
 
 module.exports = Facsimile;
--- src/object.js
+++ src/object.js
@@ -1,15 +1,23 @@
-const { guid } = require("./guid");
+const RESET_CALLS = new Set([
+    Array.prototype.sort
+]);
 
 class ObjectReference {
-    constructor(parent, object, id = guid()) {
-        this._vectors = new Map();
+    constructor(parent, object, guid = ObjectReference.guid()) {
+        this._guid = guid;
+        this._vectors = {};
         this._parent = parent;
         this._storage = storage;
         this._proxy = new Proxy(this._storage, this);
 
         Object.freeze (this);
 
-        parent.register(id, this);
+        parent.register(guid, this);
+    }
+
+    static guid() {
+        console.log("!!!");
+        return `${this._parent._hostname} ${crypto.randomUUID()}`
     }
 
     static as(parent, type, id) {
@@ -17,34 +25,80 @@ class ObjectReference {
     }
 
     static from(parent, object, id) {
-        if (Array.isArray(object) && Object.getPrototypeOf(object) !== Array.prototype) {
+        if (typeof object !== 'object') {
+            return object;
+        } if (Array.isArray(object) && Object.getPrototypeOf(object) !== Array.prototype) {
             throw new Error("Cannot coerse sub-classed array into a tracked object reference");
         } else if (Object.getPrototypeOf(object) !== Object.prototype) {
             throw new Error("Can only coerse basic objects and arrays to object reference");
         }
 
-        const ref = new ObjectReference(parent, object, id);
+        return new ObjectReference(parent, object, id);
     }
 
     get proxy() {
         return this._proxy;
     }
 
+    // Private members
+    _send(op, key, args) {
+        const [index] = this._vectors[key] || [-1];
+        const vector = [index+1, this._parent._hostname];
+
+        this._parent.send({
+            op,
+            vector,
+            ... args
+        })
+    }
+
+    _serialize () {
+        // TODO: Create a network safe copy of the stored value
+    }
+
     // Proxy traps
-    setPrototypeOf() {
-        throw new Error("Cannot assign new ")
+    getPrototypeOf () {
+        return Object.getPrototypeOf(this._storage)
     }
 
-    get() {
-        // TODO: HOOK ARRAY FUNCTIONS
+    setPrototypeOf () {
+        throw new Error("Cannot override prototypes of tracked objects")
     }
 
-    set() {
+    get (key) {
+        const value = this._storage[key];
+        const that = this;
+
+        if (RESET_CALLS.has(value)) {
+            return function (... rest) {
+                const ret = value.apply(this, rest);
+                this._parent.send('reset', key, this._serialize());
+            }
+        } if (typeof value === 'function') {
+            return function (... rest) {
+                const ret = value.apply(this, rest);
+                this._parent.send('reset', key, this._serialize());
+            }
+        } else if (typeof value === 'object' && object !== null) {
+            return this._parent.lookup(value);
+        } else {
+            return value;
+        }
+    }
+
+    set (key, value) {
 
     }
 
-    delete() {
-
+    delete (key) {
+        switch (typeof this._storage[key]) {
+            case 'undefined':
+            case 'function':
+                return ;
+            default:
+                this._parent.send('delete', this._id, key);
+                delete this._storage[key];
+        }
     }
 }
 
